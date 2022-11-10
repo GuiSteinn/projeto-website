cce4300ebe252d1948752a3f7954537e12026cb6
GuiSteinn <guilhermezuninosgrott@gmail.com>
2022-11-10 13:30:13 -0300

    ok

---
+++ unifique-clone/token.js
@@ -0,0 +1,7 @@
+function getUrlBase(port) {
+    return "https://uypbkefvmyelhgfazpvs.supabase.co/rest/v1/" + port + "select=*";
+}
+
+function getTokenSupabase() {
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5cGJrZWZ2bXllbGhnZmF6cHZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMwMDEzNDYsImV4cCI6MTk3ODU3NzM0Nn0.jReZnfPVeeWQWU9H5kxo2VOg5q8HlmLdBrN9lC94_NI";
+}
